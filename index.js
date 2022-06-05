1ca72fdbb31f6e8e493a861f20dfb8772de99a98
coderdev27 <99113685+coderdev27@users.noreply.github.com>
2022-06-05 16:17:22 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,97 @@
+var robotjs = require("robotjs");
+var ncp = require("copy-paste");
+// var express = require("express")
+
+// var app = express()
+
+
+// app.get("/",(req,res)=>{
+
+  (async () =>{
+    robotjs.moveMouse(272,749)
+    robotjs.mouseClick()
+
+    setTimeout(()=>{
+      robotjs.moveMouse(568,515)
+      robotjs.mouseClick()
+          
+          setTimeout(()=>{
+            robotjs.moveMouse(766,415)
+            robotjs.mouseClick()
+            
+               setTimeout(()=>{
+                robotjs.moveMouse(463,495)
+                robotjs.mouseClick()
+                
+                setTimeout(()=>{
+                  robotjs.moveMouse(470,498)
+                  robotjs.mouseClick()
+                  setTimeout(()=>{
+                    robotjs.moveMouse( 474,617)
+                    robotjs.mouseClick()
+                    setTimeout(()=>{
+                      robotjs.moveMouse( 416,486)
+                      robotjs.mouseClick()
+                      setTimeout(()=>{
+                        robotjs.moveMouse( 501, 411)
+                        robotjs.mouseClick()
+                        setTimeout(()=>{
+                          robotjs.moveMouse( 441, 263)
+                          robotjs.mouseClick()
+                          setTimeout(()=>{
+                            robotjs.moveMouse(  472 ,377)
+                            robotjs.mouseClick()
+                            setTimeout(()=>{
+                              robotjs.moveMouse(  460, 512)
+                              robotjs.mouseClick()
+                              setTimeout(()=>{
+                                robotjs.moveMouse(   471, 563)
+                                robotjs.mouseClick()
+                                setTimeout(()=>{
+                                  robotjs.moveMouse( 183,345)
+                                  robotjs.mouseClick()
+                                  setTimeout(()=>{
+                                    robotjs.moveMouse(562,354)
+                                    robotjs.mouseClick()
+
+                                    // setTimeout(()=>{
+                                    //   res.send("Code Generated")
+                                         
+                                    //  },2000)
+                                       
+                                   },2000)
+                                 },2000)
+                               },2000)
+                             },2000)
+                           },2000)
+                         },2000)
+                       },2000)
+                     },2000)
+                   },2000)
+                 },2000)
+
+               },32000)
+
+          },3500)
+          
+    },2000)
+    
+   
+    
+    })()
+
+
+// })
+
+// app.get("/api",(req,res)=>{
+//   res.send("aapi")
+// })
+
+// app.listen(80,()=>{
+//   console.log("Listening on 80");
+// })
+  
+
+
+
+    
